7d61665bcc1a6a161e7d3f2f926d71615b227277
ArchLeaders <archparliament@gmail.com>
2023-07-10 19:10:54 -0700

    Add CeadConfig

---
+++ src/CeadConfig.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ConfigFactory.Core;
+using ConfigFactory.Core.Attributes;
+
+namespace NxEditor.CeadPlugin;
+
+public partial class CeadConfig : ConfigModule<CeadConfig>
+{
+    [ObservableProperty]
+    [property: Config(Header = "RESTBL Strings", Description = "", Category = "EAD Plugin")]
+    private string _restblStrings = string.Empty;
+
+    partial void OnRestblStringsChanged(string value)
+    {
+        SetValidation(() => RestblStrings,
+            value => !string.IsNullOrEmpty(value));
+    }
+}
