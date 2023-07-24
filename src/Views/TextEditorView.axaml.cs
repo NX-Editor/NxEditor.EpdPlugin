5cec18b18e0733b1c48d3f283608f8dc231ca82a
ArchLeaders <archleadership28@gmail.com>
2023-07-24 00:02:31 -0700

    Handle dark/light text themes; impl high-level control (TextEditor)

--- src/Views/TextEditorView.axaml.cs
+++ src/Views/TextEditorView.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Styling;
 using AvaloniaEdit.TextMate;
 using NxEditor.EpdPlugin.ViewModels;
 using TextMateSharp.Grammars;
@@ -7,23 +9,43 @@ namespace NxEditor.EpdPlugin.Views;
 
 public partial class TextEditorView : UserControl
 {
-    private static readonly RegistryOptions _registryOptions = new(ThemeName.DarkPlus);
-    private static string _grammerId = string.Empty;
+    private static RegistryOptions _registryOptions = new(ThemeName.DarkPlus);
+
+    public string GrammarId { get; set; } = string.Empty;
 
     public TextEditorView()
     {
         InitializeComponent();
+
+        Application.Current!.PropertyChanged += (s, e) => {
+            if (e.Property == Application.RequestedThemeVariantProperty) {
+                _registryOptions = new((ThemeVariant)e.NewValue! == ThemeVariant.Dark
+                    ? ThemeName.DarkPlus : ThemeName.LightPlus);
+                TrySetGrammarId();
+                ReloadSyntaxHighlighting();
+            }
+        };
     }
 
     protected override void OnDataContextChanged(EventArgs e)
     {
         base.OnDataContextChanged(e);
 
+        TrySetGrammarId();
+        ReloadSyntaxHighlighting();
+    }
+
+    public void ReloadSyntaxHighlighting()
+    {
+        TextMate.Installation textMateInstallation = TextEditor.InstallTextMate(_registryOptions);
+        textMateInstallation.SetGrammar(GrammarId);
+    }
+
+    private void TrySetGrammarId()
+    {
         if (DataContext is TextEditorViewModel textEditorViewModel) {
             if (_registryOptions.GetLanguageByExtension(Path.GetExtension(textEditorViewModel.Handle.Path))?.Id is string id) {
-                _grammerId = _registryOptions.GetScopeByLanguageId(id);
-                TextMate.Installation textMateInstallation = TextEditor.InstallTextMate(_registryOptions);
-                textMateInstallation.SetGrammar(_grammerId);
+                GrammarId = _registryOptions.GetScopeByLanguageId(id);
             }
         }
     }
