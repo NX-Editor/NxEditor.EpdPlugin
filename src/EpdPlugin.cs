a3ab06d377a3d09e5c41b2db578f231df4539bdb
ArchLeaders <archleadership28@gmail.com>
2024-01-01 16:04:27 -0800

    Use MessageStudio for Msbt editor

--- src/EpdPlugin.cs
+++ src/EpdPlugin.cs
@@ -1,5 +1,4 @@
-﻿using CsMsbt;
-using CsOead;
+﻿using CsOead;
 using CsRestbl;
 using Native.IO.Services;
 using NxEditor.EpdPlugin.Processors;
@@ -18,12 +17,10 @@ public class EpdPlugin : IServiceExtension
     {
         NativeLibraryManager.RegisterAssembly(typeof(EpdPlugin).Assembly, out bool isCommonLoaded)
             .Register(new OeadLibrary(), out bool isOeadLoaded)
-            .Register(new MsbtLibrary(), out bool isMsbtLoaded)
             .Register(new RestblLibrary(), out bool isRestblLoaded);
 
         Console.WriteLine($"Loaded native_io: {isCommonLoaded}");
         Console.WriteLine($"Loaded cs_oead: {isOeadLoaded}");
-        Console.WriteLine($"Loaded cs_msbt: {isMsbtLoaded}");
         Console.WriteLine($"Loaded cs_restbl: {isRestblLoaded}");
 
         IconProvider.Current.Register(new MaterialSymbolIconProvider());
--- src/ViewModels/MsbtEditorViewModel.cs
+++ src/ViewModels/MsbtEditorViewModel.cs
@@ -1,4 +1,4 @@
-﻿using CsMsbt;
+﻿using MessageStudio.Formats.BinaryText;
 using NxEditor.EpdPlugin.Models.Common;
 using NxEditor.PluginBase.Models;
 
@@ -23,14 +23,14 @@ public class MsbtEditorViewModel : TextEditorBase
     public override Task Read()
     {
         Msbt msbt = Msbt.FromBinary(Handle.Data);
-        View.TextEditor.Text = msbt.ToText().ToString();
+        View.TextEditor.Text = msbt.ToYaml();
 
         return Task.CompletedTask;
     }
 
     public override Task<IFileHandle> Write()
     {
-        Msbt msbt = Msbt.FromText(View.TextEditor.Text);
+        Msbt msbt = Msbt.FromYaml(View.TextEditor.Text);
         Handle.Data = msbt.ToBinary().ToArray();
         return Task.FromResult(Handle);
     }
